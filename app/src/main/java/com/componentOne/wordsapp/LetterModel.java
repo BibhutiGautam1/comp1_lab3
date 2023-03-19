ab49a80f98d6a0b3ef83adb817d2aa0979ff4ae0
BibhutiGautam1 <gbibhuti20@tbc.edu.np>
2023-03-19 18:01:12 +0000

    Initial Load

---
+++ app/src/main/java/com/componentOne/wordsapp/LetterModel.java
@@ -0,0 +1,9 @@
+package com.componentOne.wordsapp;
+
+public class LetterModel {
+    String letter;
+
+    public LetterModel(String letter) {
+        this.letter = letter;
+    }
+}
---
+++ app/src/main/java/com/componentOne/wordsapp/WordModel.java
@@ -0,0 +1,9 @@
+package com.componentOne.wordsapp;
+
+public class WordModel {
+    String word;
+
+    public WordModel(String word) {
+        this.word = word;
+    }
+}
